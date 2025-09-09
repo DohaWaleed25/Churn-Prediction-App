fe3dac82504f23f5d9e22f3f031b9cf16c48bc07
Doha Waleed <dohakamal1973@gmail.com>
2025-09-09 16:45:58 +0300

    Update Doha_Proj2Deployment.py

--- Doha_Proj2Deployment.py
+++ Doha_Proj2Deployment.py
@@ -15,7 +15,7 @@ page_bg = """
     background-color: rgba(0,0,0,0);
 }
 [data-testid="stSidebar"] {
-    background: #87AFC7;
+    background: #95BDC7;
 }
 </style>
 """
@@ -86,6 +86,7 @@ with col2:
     ax.axis("equal")
 
     st.pyplot(fig)
+
 
 
 
